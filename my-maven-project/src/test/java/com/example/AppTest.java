ee971d3e1849aded04bdd19c3ad5755be76d20e7
AdeshM12 <trivedipradum96@gmail.com>
2025-02-15 14:29:26 +0530

    maven_compilation_success

--- my-maven-project/src/test/java/com/example/AppTest.java
+++ my-maven-project/src/test/java/com/example/AppTest.java
@@ -1,38 +1,14 @@
-package com.example;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
     /**
-     * @return the suite of tests being tested
+     * Testing app.java file :-)
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testApp() {
+        assertTrue(true);
     }
 }
